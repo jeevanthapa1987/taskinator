d930f0966a312eb59755a0eae6cb98bb3f77a178
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2021-12-27 14:10:35 -0500

    refactor the code

---
+++ this.py
@@ -0,0 +1,22 @@
+event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    //console.dir(taskNameInput);
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    console.dir(listItemEl);
+ // create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+// give it a class name
+taskInfoEl.className = "task-info";
+// add HTML content to div
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+listItemEl.appendChild(taskInfoEl);
+    //listItemEl.textContent = "taskNameInput.";
+    tasksToDoEl.appendChild(listItemEl);
+  }
+  //buttonEl.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit",taskFormHandler );
