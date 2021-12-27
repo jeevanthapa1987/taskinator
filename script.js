2d19fd81c2fe2cff37714b601f2e7353533dbee7
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2021-12-27 13:04:58 -0500

    handle form submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var buttonEl = document.querySelector("#save-task");
+//var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //buttonEl.addEventListener("click", function() {
@@ -8,11 +9,15 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
   //tasksToDoEl.appendChild(listItemEl);
 //});
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
   }
-  buttonEl.addEventListener("click", createTaskHandler);
+  //buttonEl.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
 
