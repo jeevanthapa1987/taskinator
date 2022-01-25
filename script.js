7f37553c7a9945887e7d9cc32c435d6f506cf77b
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2022-01-25 16:40:15 -0500

    capture form field values

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 var formEl=document.querySelector("#task-form");
 
-var buttonEl = document.querySelector("#save-task");
+//var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler=function(event){
     event.preventDefault();
+    var taskNameInput=document.querySelector("input[name='task-name']").value;
+    var taskTypeInput=document.querySelector("select[name='task-type']").value;
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    var taskInfoEl=document.createElement("div");
+    taskInfoEl.className="task-info";
+taskInfoEl.innerHTML="<h3 class='task-name'>" +taskNameInput+"</h3><span class='task-type'>"+taskTypeInput+"</span>";
+//taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+listItemEl.appendChild(taskInfoEl);
+   // listItemEl.textContent = "taskNameInput";
     tasksToDoEl.appendChild(listItemEl);   
 }
-buttonEl.addEventListener("click",createTaskHandler);
+//buttonEl.addEventListener("click",createTaskHandler);
 formEl.addEventListener("submit",createTaskHandler);
