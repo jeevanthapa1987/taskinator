b4e6c1422ac764ee4f5d54426faf9407758bed6e
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2024-02-23 14:38:01 -0500

    add the task form

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ var createTaskHandler = function() {
   listItemEl.className = "task-item"; 
   listItemEl.textContent = "This is a new task."; 
   tasksToDoEl.appendChild(listItemEl);
+
   }; 
 
 buttonEl.addEventListener("click", createTaskHandler);
+
+
